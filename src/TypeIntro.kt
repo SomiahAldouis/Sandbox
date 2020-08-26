777515a57f6a24ec8464545d4b9bfb2e28a5098b
somiahgit <somiahjamal1997@gmail.com>
2020-08-26 08:00:35 +0300

    Initial commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,16 @@
+
+fun main(){
+    var experienceType = 5
+    val PlayerName = "Estragon"
+
+    experienceType+=5
+
+    println(PlayerName)
+    println(experienceType)
+
+    var hasSteed = 0
+    println(PlayerName.reversed())
+}
+
+class TypeIntro {
+}
